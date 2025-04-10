634d21b149a039705c42792a33df78b1f6bb310a
Felipemz01 <ingfelipemz@gmail.com>
2025-04-10 00:23:37 -0500

    Cambios para Angular Deploy2

--- GymPlus/urls.py
+++ GymPlus/urls.py
@@ -1,30 +1,10 @@
-"""
-URL configuration for GymPlus project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('miembros.urls')),  # <-- Cambia 'tu_app' por el nombre real de tu app
-    re_path(r'^.*$', never_cache(TemplateView.as_view(template_name='index.html'))),
+    path('api/', include('miembros.urls')),  # Todas tus rutas de API REST
+    re_path(r'^.*$', never_cache(TemplateView.as_view(template_name="index.html"))),
 ]
-
-
-
--- miembros/urls.py
+++ miembros/urls.py
@@ -8,8 +8,9 @@ from .views import LoginView,HorariosDisponiblesView,reservar_clase,LogoutView,c
 
 urlpatterns = [
     path('api/miembros/', views.registrar_miembro, name='registrar_miembro'),
+    path('api/login/', views.login_api_view, name='login'),
     #path('login/', LoginView.as_view(), name='login'),
-    path('login/', lambda request: redirect('/')),
+    #path('login/', lambda request: redirect('/login/')),
     path('horarios/', HorariosDisponiblesView.as_view(), name='horarios_disponibles'),
     path('reservar/', reservar_clase, name='reservar_clase'),
     path('logout/', LogoutView.as_view(), name='logout'),
