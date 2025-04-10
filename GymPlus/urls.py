e680096e08552164633b528fe8a824bc52c2a264
Felipemz01 <ingfelipemz@gmail.com>
2025-04-10 00:41:34 -0500

    Cambios para Angular Deploy4

--- GymPlus/urls.py
+++ GymPlus/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('miembros.urls')),  # Todas tus rutas de API REST
-    re_path(r'^.*$', never_cache(TemplateView.as_view(template_name="index.html"))),
-]
+    path('', TemplateView.as_view(template_name='index.html')),  # Sirve Angular
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
